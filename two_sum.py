b89ab98d9cc45ab9b0e6b39b60ef424ee985ab05
IslamKurbanov <ik509331@gmail.com>
2023-12-07 20:27:28 +0600

    two sums

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,4 @@
+# Brute Force
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         n = len(nums)
@@ -5,5 +6,17 @@ class Solution:
             for j in range(i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-obj = Solution()
-print(obj.twoSum([2,7,11,15], 9))
+
+# Dart code
+"""class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    for(int i = 0; i < nums.length; i++){
+        for(int j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i, j];
+            }
+        }
+    } 
+    return [];
+  }
+}"""
